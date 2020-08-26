b90d0cb0ccbb1c20161097a2b3e0d2005d9fdf7e
Денис <denis.sokolow@gmail.com>
2020-08-26 20:26:57 +0300

    master

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 const PORT = 80;
 const server = http.createServer();
 server.listen(process.env.PORT || PORT);
 server.on('request', (req, res) => {
-    switch(req.url) {
-        case '/static':{
-            res.writeHead(200, {
-                'Content-Type': 'application/pdf; charset=utf-8',
-                'Access-Control-Allow-Origin': '*'
-            });
-            fs.readFile('./files/static.pdf', (err, data) => { 
-                if (err) {
-                    console.error(err)
-                    return
-                }
-            res.end(data)
-            })
-           }
-        case '/primer': {
+    if (req.url === '/text') {
             res.writeHead(200, {
                 'Content-Type': 'text/ html; charset=utf-8',
                 'Access-Control-Allow-Origin': '*'
             });
-            fs.readFile('./files/primer.txt', (err, data) => {
+            fs.readFile(path.join(__dirname, 'files', 'primer.txt'), (err, data) => {
                 if (err) {
                     console.error(err)
                     return
                 }
                 res.end(data)
             })
-        }
-        default: {
+    }else if (req.url === '/pdf'){
+            res.writeHead(200, {
+                'Content-Type': 'application/pdf; charset=utf-8',
+                'Access-Control-Allow-Origin': '*'
+            });
+            fs.readFile(path.join(__dirname, 'files', 'static.pdf'), (err, data) => { 
+                if (err) {
+                    console.error(err)
+                    return
+                }
+            res.end(data)
+            })
+    } else {       
             res.writeHead(200, {
                 'Content-Type': 'text/html; charset=utf-8',
                 'Access-Control-Allow-Origin': '*'
@@ -43,6 +41,6 @@ server.on('request', (req, res) => {
                 }
                 res.end(data)
             })
-        }
+        
     }
 })
