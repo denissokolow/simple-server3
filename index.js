1331ee55c05883da4959da57f1d3544af1d454a1
Денис <denis.sokolow@gmail.com>
2020-08-26 19:37:13 +0300

    master

--- index.js
+++ index.js
@@ -4,26 +4,45 @@ const PORT = 80;
 const server = http.createServer();
 server.listen(process.env.PORT || PORT);
 server.on('request', (req, res) => {
-
     switch(req.url) {
         case '/static':{
             res.writeHead(200, {
-                'Content-Type': 'text/javascript; charset=utf-8', //испр контент тайп
+                'Content-Type': 'application/pdf; charset=utf-8',
                 'Access-Control-Allow-Origin': '*'
             });
-            fs.readFile('/Users/flavio/test.txt', (err, data) => { //испр адрес
+            fs.readFile('./files/static.pdf', (err, data) => { 
+                if (err) {
+                    console.error(err)
+                    return
+                }
+            res.end(data)
+            })
+           }
+        case '/primer': {
+            res.writeHead(200, {
+                'Content-Type': 'text/ html; charset=utf-8',
+                'Access-Control-Allow-Origin': '*'
+            });
+            fs.readFile('./files/primer.txt', (err, data) => {
                 if (err) {
                     console.error(err)
                     return
                 }
                 res.end(data)
             })
-
         }
-
+        default: {
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8',
+                'Access-Control-Allow-Origin': '*'
+            });
+            fs.readFile('./files/index.html', (err, data) => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+                res.end(data)
+            })
+        }
     }
-   
-
-
-
 })
